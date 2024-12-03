9af48e2e0dd2f3a93dbc37d18bfa5a84d5ae0c7a
BerkayUslu <81648931+BerkayUslu@users.noreply.github.com>
2024-12-03 14:35:44 +0300

    Update ObstacleBase.cs

--- Platform Runner/Assets/Scripts/Obstacles/ObstacleBase.cs
+++ Platform Runner/Assets/Scripts/Obstacles/ObstacleBase.cs
@@ -2,24 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace  PlatformRunner
+namespace PlatformRunner
 {
     public class ObstacleBase : MonoBehaviour, IObstacle
     {
-        private void KillCollidedObject(Collider other)
-        {
-            if (other.TryGetComponent(out IHealth health))
-            {
-                health.KillCharacter();
-            }
-        }
-        
         protected bool IsEnemOrPlayer(Collider other) =>
             other.CompareTag(Tags.Player) || other.CompareTag(Tags.Enemy);
 
         public virtual void TryKillCollidedObject(Collider other)
         {
-            KillCollidedObject(other);
+            if (other.TryGetComponent(out IHealth health))
+            {
+                health.KillCharacter();
+            }
         }
     }
 }
